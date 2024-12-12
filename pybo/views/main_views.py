469fc99aabb1c499eb21588000cf766a471563d1
choiseokyong <ggt9875654@naver.com>
2024-12-12 16:23:38 +0900

    질문 목록과 질문 상세 기능 만들기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+# models.py에 Question 클래스를 가져와서 사용
+from pybo.models import Question
 
 # 객체 bp
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -14,4 +16,16 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return 'Pybo index'
+    # 밑에 코드가 dbquestion 테이블 정보를 가져옴
+    question_list = Question.query.order_by(Question.create_date.desc())
+    # render_template("html 파일 경로",html에 변수 = python 변수)
+    # db에서 query해서 가져온 question_list를 question_list.html 안에
+    #question_list에 대입
+    return render_template('question/question_list.html', question_list=question_list)
+
+@bp.route('/detail/<int:question_id>/')
+def detail(question_id):
+    # DB에서 qeustion_id로 question 자료를 가져옴
+    question = Question.query.get(question_id)
+    # render_template : DB에서 가져온 question을 템플릿에 입력
+    return render_template('question/question_detail.html', question=question)
