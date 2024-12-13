578a22632523037f60af1ec7ad4182b73a524e44
choiseokyong <ggt9875654@naver.com>
2024-12-13 11:48:43 +0900

    mysql + flask 연결 shell로 데이터 생성

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, render_template
 # models.py에 Question 클래스를 가져와서 사용
 from werkzeug.utils import redirect
 
@@ -19,3 +19,14 @@ def index():
    return redirect(url_for('question._list'))
 
 
+@bp.route('/test')
+def test():
+    # 관련 웹페이지 주소
+    # 1. redirect('페이지url'))
+    # 2. render_template() 페이지를 template 사용하는 경우 사용
+    # 3. redirect(url_for('다른 라우트에 있는 페이지'))
+    return render_template("test.html")
+
+@bp.route('/test2')
+def test2():
+    return render_template("./test/test2.html")
